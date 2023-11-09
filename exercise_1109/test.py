b594c601c2780e4bfa090c6702dc279d3c4eee11
siang1124 <150311583+siang1124@users.noreply.github.com>
2023-11-09 11:51:41 +0800

    test.py

--- exercise_1109/test.py
+++ exercise_1109/test.py
@@ -25,4 +25,46 @@ def show_employee(name,salary=80000):
     print("Name:",name,"salary:",salary)
 show_employee("Ben",12000)
 show_employee("Jessa")
-show_employee("hu")
+show_employee("Hu")
+
+# outer function
+def outer_fun(a, b):
+    square = a ** 2
+
+    # inner function
+    def addition(a, b):
+        return a + b
+
+    # call inner function from outer function
+    add = addition(a, b)
+    # add 5 to the result
+    return add + 5
+
+result = outer_fun(5, 10)
+print(result)
+
+def addition(num):
+    if num:
+        # call same function by reducing number by 1
+        return num + addition(num - 1)
+    else:
+        return 0
+
+res = addition(10)
+print(res)
+
+def display_student(name, age):
+    print(name, age)
+
+# call using original name
+display_student("Emma", 26)
+
+# assign new name
+showStudent = display_student
+# call using new name
+showStudent("Emma", 26)
+
+print(list(range(4,30,2)))
+
+x = [4,6,8,24,12,2]
+print(max(x))
